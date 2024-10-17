3c3b3038be98e48c044ebc11f75590ce04963b3b
Abdulwasay098671 <abdulwasay.09867.1@gmail.com>
2024-10-17 07:28:03 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function telltime() {
+    let date = new Date()
+    let h = date.getHours()
+    let m = date.getMinutes()
+    let s = date.getSeconds()
+    
+    console.log(h , m , s)
+}
+telltime()
